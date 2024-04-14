62e05254e6058e54281ebd0430d9f3e9e3c0153b
unknown <ks.monish@hotmail.com>
2024-04-14 22:53:09 +0530

    Added Submit form and dbms connection

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template,jsonify
-from database import load_from_db
+from flask import Flask, render_template,jsonify,request
+from database import load_from_db, load_fruit_from_db,insert_into_database
 
 app = Flask(__name__)
 
@@ -12,6 +12,43 @@ def hello_world():
 def list_items():
     food_items = load_from_db()
     return jsonify(food_items)
+
+@app.route('/submit_form', methods=['GET', 'POST'])
+def submit_form():
+    if request.method == 'GET':
+        return render_template('add-inventory.html')
+    elif request.method == 'POST':
+        form_data = request.json
+        name = form_data.get("inputName")
+        category = form_data.get("inputCategory")
+        quantity = int(form_data.get("inputQuantity"))
+        exp_date = form_data.get("inputExpDate")
+        supplier_id = form_data.get("inputSupplierID")
+
+        form_data_send = {
+            "name" : name,
+            "category" : category,
+            "quantity" : quantity,
+            "exp_date" : exp_date,
+            "supplier_id" : supplier_id
+        }
+
+        value = insert_into_database(form_data_send)
+        if value:
+            print("successful")
+        else:
+            print("error")
+        return 'Form Submitted successfully'
+
+
+@app.route("/<id>")
+def show_fruit(id):
+    fruit = load_fruit_from_db(id)
+    if fruit is None:
+        return jsonify({"error": "Fruit not found"}), 404
+    else:
+        return jsonify(fruit)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -9,3 +9,30 @@ def load_from_db():
         for row in result.all():
             food_items.append(dict(row._mapping))
         return food_items
+
+def load_fruit_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text(f"select * from inventory WHERE ItemID = '{id}'"))
+        result_all = result.all()
+        column_names = result.keys()
+        rows = []
+        for row in result_all:
+            rows.append(dict(zip(column_names,row)))
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]
+
+
+def insert_into_database(data):
+    try:
+        with engine.connect() as conn:
+            query = text(
+                    f"INSERT INTO inventory (Name, Category, Quantity, ExpDate, SupplierID) VALUES ('{data['name']}', '{data['category']}', {data['quantity']}, '{data['exp_date']}', '{data['supplier_id']}')"
+                )
+            conn.execute(query)
+            conn.commit()  # Commit changes to the database
+        return True  # Return True if insertion is successful
+    except Exception as e:
+        print(f"Error inserting data into database: {e}")
+        return False  # Return False if an error occurs during insertion
